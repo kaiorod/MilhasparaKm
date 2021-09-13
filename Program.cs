d9379b5e37197c7ced494d8a75bcf923b43fddd1
kaiorod <caiorodrigo2775@gmail.com>
2021-09-12 21:02:18 -0300

    Finaliza MilhasparKm

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MilhasparaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ml, convkm;
+
+            Console.Write("Digite a medida em Milhas: ");
+            ml = double.Parse(Console.ReadLine());
+
+            convkm = ml * 1.609;
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine($"{convkm} Km");
+            Console.ResetColor();
+        }
+    }
+}
